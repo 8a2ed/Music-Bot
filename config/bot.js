af503be00911604ed1362c64aa2a4c086ef9f66f
8a2ed <56230275+8a2ed@users.noreply.github.com>
2021-06-10 19:10:04 +0200

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'MusicBot by 8a2ed',
+        token: 'NzI4NzQzMjM5OTgwNDE3MTQ3.Xv-1Bw.ooDSyug0jS6X9hD0Ceo_daVH5yo',
+        prefix: '-',
+        activity: '-help',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
